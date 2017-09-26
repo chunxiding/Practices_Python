10bc84ee1e4ba49c93da45095563d5a1eb7e4f13
Chunxi Ding <32100617+chunxiding@users.noreply.github.com>
2017-09-25 23:33:19 -0400

    Create Unique_Paths.py
    9/25/2017

---
+++ Unique_Paths.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        
+        
+        # build a list to contain all paths
+        dp = [[1 for _ in xrange(n)] for _ in xrange(m)]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[-1][-1]
